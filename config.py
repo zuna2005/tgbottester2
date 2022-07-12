77b4ea159b0012adaee60ed64a936ffe381bcb1d
zuna2005 <nasiba.zulunova@gmail.com>
2022-07-13 03:49:23 +0600

    add user to db

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = '5324843306:AAHU0HP1IaPUnCVKHkEW9ZPQzF7_EUTZb9U'
 APP_URL = 'https://tgbottester2.herokuapp.com/' + BOT_TOKEN
+DB_URI = 'postgres://qtqfbidejncnuz:e975500bb448f1b0e0e04e3eaacdfdf0775081ea6a4c9f1434219dfab9e48c91@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d2lhih5iauaalt'
