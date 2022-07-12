7b14f104df89be450d2e908ccf54ccb0d2c71f68
zuna2005 <nasiba.zulunova@gmail.com>
2022-07-13 04:14:13 +0600

    create a counter func

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ db_connection = psycopg2.connect(DB_URI, sslmode='require')
 db_object = db_connection.cursor()
 
 
+def update_messages_count(user_id):
+    db_object.execute(f"UPDATE users SET messages = messages + 1 WHERE id = {user_id}")
+    db_connection.commit()
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     user_id = message.from_user.id
@@ -26,6 +31,14 @@ def start(message):
     if not result:
         db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
         db_connection.commit()
+
+    update_messages_count(user_id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def message_from_user(message):
+    user_id = message.from_user.id
+    update_messages_count(user_id)
 
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
